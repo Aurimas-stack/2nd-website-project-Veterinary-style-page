ea9e9a963c3a1f0eac48d751eae37c659f88f016
Aurimas-stack <82368117+Aurimas-stack@users.noreply.github.com>
2021-08-23 11:01:31 +0300

    Add files via upload

---
+++ views/home/content.php
@@ -0,0 +1,105 @@
+<section class="hero">
+        <div class="hero-background">
+        <div class="container flex-container">
+            <div class="hero-titles flex-container">
+                <h3>VETERINARIAN</h3>
+                <h1>TheBestVet</h1>
+                <p>Praesent sapien massa, convallis a pellentesque nec, non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.</p>
+                <div class="buttons flex-container">
+                    <button class="blue-button">View all services</button>
+                    <button class="grey-button">Make an appointment</button>
+                </div>
+            </div>
+            <div class="hero-information flex-container">
+                <h3>Contact Us Anytime, 7 days a Week</h3>
+                <div class="flex-container">
+                    <p>CONTACT INFO</p>
+                    <div class="hero-icons flex-container">
+                        <i id="map" class="fas fa-map-marker-alt"></i>
+                        <a id="change_color1" href="http://maps.google.com/?q=1234 Green St. Washington DC 95253" target="_blank">1234 Green St. Washington DC 95253</a>
+                    </div>
+                    <div class="hero-icons flex-container">
+                        <i id="phone" class="fas fa-phone-alt"></i>
+                        <a id="change_color2" href="tel:5552548458">(555) 254-8458</a>      
+                    </div>
+                </div>
+                <div class="flex-container">
+                    <p>OPEN HOURS</p>
+                    <div class="hero-icons flex-container">
+                        <i class="fas fa-clock"></i>
+                        <p>M-F: 8am - 6pm</p>
+                    </div>
+                    <div class="hero-icons flex-container">
+                        <i class="fas fa-clock"></i>
+                        <p>S-S: 8.30am - 4pm</p>      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+    <section class="about-us">
+        <div class='container flex-container'>
+            <div class="cat-img-box">
+                <img src="images/cat_1200getty.jpg" alt="cat_photo">
+            </div>
+            <div class="us-text">
+                <div class="us-text-box">
+                    <p>VIVAMUS SUSCIPIT TORTOR</p>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class="us-text-box">
+                    <p>LOREM IPSUM DOLOR</p>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class="us-text-box">
+                    <p>MAURIS BLANDIT</p>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="home-services">
+        <div class="container flex-container">
+            <h3>Services</h3>
+            <h2>What We Do</h2>
+            <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta. </p>
+            <button class="services-button"><span class="button-arrow">ALL VET SERVICES </span></button>
+            <div class="type-of-services flex-container">
+                <div class="services-row flex-container">
+                    <div class="service-description flex-container">
+                        <img src="images/veterinarian-icon-1.png" alt="vet_bottle">
+                        <h3>MAURIS BLANDIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                    <div class="service-description flex-container">
+                        <img src="images/veterinarian-icon-12.png" alt="dog_house">
+                        <h3>CONSECTETUR</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                    <div class="service-description flex-container">
+                        <img src="images/dog_help.png" alt="dog_red_cross">
+                        <h3>NULLA SIT AMET</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                </div>
+                <div class="services-row flex-container">
+                    <div class="service-description flex-container">
+                        <img src="images/phone_ve.png" alt="phone_vet">
+                        <h3>AMET SIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                    <div class="service-description flex-container">
+                        <img src="images/drugs.png" alt="drugs">
+                        <h3>VIVAMUS SUSCIPIT TORTOR</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                    <div class="service-description flex-container">
+                        <img src="images/syringe.png" alt="syringe">
+                        <h3>MAURIS BLANDIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
