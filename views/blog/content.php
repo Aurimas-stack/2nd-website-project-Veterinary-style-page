2f8ceca6f50e92d62d9f43bfcf0690cd7647c247
Aurimas-stack <82368117+Aurimas-stack@users.noreply.github.com>
2021-08-23 11:00:02 +0300

    Add files via upload

---
+++ views/blog/content.php
@@ -0,0 +1,125 @@
+<section class='hero'>
+        <div class='container flex-container'>
+            <h3>NEWS</h3>
+            <h1>Our Blog</h1>
+            <div class='slideshow-container flex-container'>
+                <div class='slider flex-container slide-in-fwd-center'>
+                    <h2> VolutPat Venenatis</h2>
+                    <div class='slider-info flex-container'>
+                        <p>by Aurimas-stack</p>
+                        <p> | </p>
+                        <p>June 6, 2021 </p>
+                        <p> | </p>
+                        <p>Category</p>
+                        <p> | </p>
+                        <p>4 Comments</p>
+                    </div>
+                    <p class="addReadMore showlesscontent">Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Pellentesque in ipsum id orci porta dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt. Proin eget tortor risus. </p>
+                    <button class="services-button">READ MORE<span class="button-arrow"></span></button>
+                </div>
+                <div class='slider flex-container slide-in-fwd-center'>
+                    <h2>Nunc VolutPat </h2>
+                    <div class='slider-info flex-container'>
+                        <p>by Aurimas-stack</p>
+                        <p> | </p>
+                        <p>July 9, 2019 </p>
+                        <p> | </p>
+                        <p>Category</p>
+                        <p> | </p>
+                        <p>10 Comments</p>
+                    </div>
+                    <p class="addReadMore showlesscontent">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Pellentesque in ipsum id orci porta dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt. Proin eget tortor risus. </p>
+                    <button class="services-button">READ MORE<span class="button-arrow"></span></button>
+                </div>
+                <div class='slider flex-container slide-in-fwd-center''>
+                    <h2>Nunc VolutPat Venenatis</h2>
+                    <div class='slider-info flex-container'>
+                        <p>by Aurimas-stack</p>
+                        <p> | </p>
+                        <p>May 7, 2018 </p>
+                        <p> | </p>
+                        <p>Category</p>
+                        <p> | </p>
+                        <p>15 Comments</p>
+                    </div>
+                    <p class="addReadMore showlesscontent">Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Pellentesque in ipsum id orci porta dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt. Proin eget tortor risus. </p>
+                    <button class="services-button">READ MORE<span class="button-arrow"></span></button>
+                </div>
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                <div class='dots'>
+                    <span class="dot" onclick="currentSlide(1)"></span> 
+                    <span class="dot" onclick="currentSlide(2)"></span> 
+                    <span class="dot" onclick="currentSlide(3)"></span>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section class="blog-windows">
+        <div class='container flex-container'>
+            <div class='window-row flex-container'>
+                <div class='blog-window'>
+                    <img src="images/download.jpg" alt='nature'>
+                    <div>
+                        <h3> NUNC VOLUTPAT VENENATIS</h3>
+                        <p>May 5, 2021 | Category</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam justo et nibh venenatis aliquet. Morbi mollis mollis pellentesque. Aenean vitae erat velit.</p>
+                    </div>
+                </div>
+                <div class='blog-window'>
+                    <img src="images/download.jpg" alt='nature'>
+                    <div>
+                        <h3> NUNC VOLUTPAT VENENATIS</h3>
+                        <p>May 5, 2021 | Category</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam justo et nibh venenatis aliquet. Morbi mollis mollis pellentesque. Aenean vitae erat velit.</p>
+                    </div>
+                </div>
+                <div class='blog-window'>
+                    <img src="images/download.jpg" alt='nature'>
+                    <div>
+                        <h3> NUNC VOLUTPAT VENENATIS</h3>
+                        <p>May 5, 2021 | Category</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam justo et nibh venenatis aliquet. Morbi mollis mollis pellentesque. Aenean vitae erat velit.</p>
+                    </div>
+                </div>      
+            </div>
+            <div class='window-row flex-container'>
+                <div class='blog-window'>
+                    <img src="images/download.jpg" alt='nature'>
+                    <div>
+                        <h3> NUNC VOLUTPAT VENENATIS</h3>
+                        <p>May 5, 2021 | Category</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam justo et nibh venenatis aliquet. Morbi mollis mollis pellentesque. Aenean vitae erat velit.</p>
+                    </div>
+                </div>
+                <div class='blog-window'>
+                    <img src="images/download.jpg" alt='nature'>
+                    <div>
+                        <h3> NUNC VOLUTPAT VENENATIS</h3>
+                        <p>May 5, 2021 | Category</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam justo et nibh venenatis aliquet. Morbi mollis mollis pellentesque. Aenean vitae erat velit.</p>
+                    </div>
+                </div>
+                <div class='blog-window'>
+                    <img src="images/download.jpg" alt='nature'>
+                    <div>
+                        <h3> NUNC VOLUTPAT VENENATIS</h3>
+                        <p>May 5, 2021 | Category</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam justo et nibh venenatis aliquet. Morbi mollis mollis pellentesque. Aenean vitae erat velit.</p>
+                    </div>
+                </div>      
+            </div>
+        </div>
+    </section>
+    <section class="email-sub">
+        <div class='container flex-container'>
+            <form class='flex-container' action='blog.php' method='post' id="blogForm">
+                <div id="turning-circle1"></div>
+                <input id='form-name-input1' type='text' name='name' placeholder="Your name" required>
+                <div id="turning-circle2"></div>
+                <input id='form-name-input2' type='email' name='email' placeholder="Your email" required>
+                <input class="services-button" value='Subscribe' type="submit" name='submit'>
+            </form>
+        </div>
+    </section>
