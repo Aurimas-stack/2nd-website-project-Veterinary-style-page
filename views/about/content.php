a4eeb30de18c9f825234a9042bc1fc3703ceaf99
Aurimas-stack <82368117+Aurimas-stack@users.noreply.github.com>
2021-08-23 10:59:30 +0300

    Add files via upload

---
+++ views/about/content.php
@@ -0,0 +1,100 @@
+<section class="about-hero">
+        <div class="container flex-container">
+            <div class="about-titles flex-container">
+                <h3>ABOUT</h3>
+                <h1>About Us</h1>
+                <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta. </p>
+            </div>
+            <div class="hero-services flex-container">
+                <div class="service flex-container">
+                    <img src="images/veterinarian-icon-7.png" alt="animal_bag">
+                    <h3>LOREM IPSUM DOLOR</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class="service flex-container">
+                    <img src="images/medical_bag.png" alt="medical_bag">
+                    <h3>VIVAMUS SUSCIPIT TORTOR</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class="service flex-container">
+                    <img src="images/veterinarian-icon-5.png" alt="fishbowl">
+                    <h3>MAURIS BLANDIT</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="our-mission">
+        <div class="container flex-container">
+            <div class="our-video flex-container">
+                <div class="flex-container">
+                    <h2>Our Mission & Vision</h2>
+                    <button class="services-button"><span class="button-arrow">ALL VET SERVICES </span></button>
+                </div>
+                <iframe class="yt-vid" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+            </div>
+            <div class="mission-text">
+                <p class="addReadMore showlesscontent">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Pellentesque in ipsum id orci porta dapibus.
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                    
+                    Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt. Proin eget tortor risus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim.
+                    
+                    Donec sollicitudin molestie malesuada. Donec rutrum congue leo eget malesuada. Donec sollicitudin molestie malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam id dui posuere blandit. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus suscipit tortor eget felis porttitor volutpat. Donec sollicitudin molestie malesuada.</p>
+            </div>
+        </div>
+    </section>
+    <section class="about-us">
+        <div class='container flex-container'>
+            <div class="cat-img-box">
+                <img src="images/cat_1200getty.jpg" alt="cat_photo">
+            </div>
+            <div class="us-text">
+                <div class="us-text-box">
+                    <p>VIVAMUS SUSCIPIT TORTOR</p>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class="us-text-box">
+                    <p>LOREM IPSUM DOLOR</p>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class="us-text-box">
+                    <p>MAURIS BLANDIT</p>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about-vets">
+        <div class="container flex-container">
+            <h3>PROFESSIONAL</h3>
+            <h1>Highly Trained Veterinarians</h1>
+            <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna </p>
+            <div class="vet-info-photo flex-container">
+                <div class="vet-photo flex-container">
+                    <img src="images/graphicstock-young-caucasian-veterinarian-with-stethoscope-examining-dogs-in-hospital-veterinarian-doctor-with-dogs-at-vet-clinic-concept-of-medicine-and-pet-care-vector-flat-design.jpg" alt="vet">
+                    <div class="flex-container">
+                        <h2>Vets Name</h2>
+                        <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, </p>
+                        <a href="mailto:theVet@mail.com">theVet@mail.com</a>
+                    </div>
+                </div>
+                <div class="vet-photo flex-container">
+                    <img src="images/graphicstock-young-caucasian-veterinarian-with-stethoscope-examining-dogs-in-hospital-veterinarian-doctor-with-dogs-at-vet-clinic-concept-of-medicine-and-pet-care-vector-flat-design.jpg" alt="vet">
+                    <div class="flex-container">
+                        <h2>Vets Name</h2>
+                        <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, </p>
+                        <a href="mailto:theVet@mail.com">theVet@mail.com</a>
+                    </div>
+                </div>
+                <div class="vet-photo flex-container">
+                    <img src="images/graphicstock-young-caucasian-veterinarian-with-stethoscope-examining-dogs-in-hospital-veterinarian-doctor-with-dogs-at-vet-clinic-concept-of-medicine-and-pet-care-vector-flat-design.jpg" alt="vet">
+                    <div class="flex-container">
+                        <h2>Vets Name</h2>
+                        <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, </p>
+                        <a href="mailto:theVet@mail.com">theVet@mail.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
