52070f6a4abb427b833b32a453c9be74a77438f8
Aurimas-stack <82368117+Aurimas-stack@users.noreply.github.com>
2021-08-23 11:02:03 +0300

    Add files via upload

---
+++ views/services/content.php
@@ -0,0 +1,88 @@
+<section class="hero">
+        <div class="hero-background">
+        <div class="container flex-container">
+            <div class="hero-titles flex-container">
+                <h3>SERVICE</h3>
+                <h1>Pet Health Maintenance</h1>
+                <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada </p>
+                <div class="buttons flex-container">
+                    <button class="blue-button">Make an appointment</button>
+                    <button class="grey-button">Chat with a doctor</button>
+                    <button class='f-a-q'>Frequently Asked Questions</button>
+                </div>
+            </div>
+            <div></div>
+        </div>
+    </div>
+    </section>
+    <section class='services-about'>
+        <div class='container flex-container'>
+            <div></div>
+            <div>
+                <h3>About</h3>
+                <h1>About General Maintenance</h1>
+                <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.</p>
+                <ul>
+                    <li>Nulla quis lorem ut</li>
+                    <li>Nulla quis lorem ut</li>
+                    <li>Nulla quis lorem ut</li>
+                    <li>Nulla quis lorem ut</li>
+                    <li>Nulla quis lorem ut</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class='services-text'>
+        <div class='container flex-container'>
+            <div class='flex-container'>
+                <div class='flex-container'>
+                    <h3>MAURIS BLANDIT</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class='flex-container'>
+                    <h3>MAURIS BLANDIT</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class='flex-container'>
+                    <h3>MAURIS BLANDIT</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+            </div>
+            <div class='flex-container'>
+                <div class='flex-container'>
+                    <h3>MAURIS BLANDIT</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class='flex-container'>
+                    <h3>MAURIS BLANDIT</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+                <div class='flex-container'>
+                    <h3>MAURIS BLANDIT</h3>
+                    <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class='review-section'>
+        <div class='container flex-container'>
+            <h3>Testimonials</h3>
+            <h1>From Our Clients</h1>
+            <div class='review-container flex-container'>
+                <div class='review flex-container'>
+                    <img src="images/istockphoto-1217828258-170667a.jpg" alt='cat photo'>
+                    <div class='review-text flex-container'>
+                        <p>Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit eli</p>
+                        <p>McCat</p>
+                    </div>
+                </div>
+                <div class='review flex-container'>
+                    <img src="images/istockphoto-1217828258-170667a.jpg" alt='cat photo'>
+                    <div class='review-text flex-container'>
+                        <p>Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit eli</p>
+                        <p>McCat</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
