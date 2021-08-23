593d8c8c1f07bb538f2672ab3197d4498e79fedd
Aurimas-stack <82368117+Aurimas-stack@users.noreply.github.com>
2021-08-23 11:00:42 +0300

    Add files via upload

---
+++ views/contact/content.php
@@ -0,0 +1,98 @@
+<section class="hero">
+        <div class="hero-background">
+        <div class="container flex-container">
+            <div class="hero-titles flex-container">
+                <h3>CONTACT</h3>
+                <h1>Contact Us</h1>
+                <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat,  </p>
+                <div class="buttons flex-container">
+                    <button class="blue-button">Send all services</button>
+                    <button class="grey-button">Book an appointment</button>
+                </div>
+            </div>
+            <div class="hero-information flex-container">
+                <h3>Contact Us Anytime, 7 days a Week</h3>
+                <div class="flex-container">
+                    <p>CONTACT INFO</p>
+                    <div class="hero-icons flex-container">
+                        <i id="map" class="fas fa-map-marker-alt"></i>
+                        <a id="change_color1" href="http://maps.google.com/?q=1234 Green St. Washington DC 95253" target="_blank">1234 Green St. Washington DC 95253</a>
+                    </div>
+                    <div class="hero-icons flex-container">
+                        <i id="phone" class="fas fa-phone-alt"></i>
+                        <a id="change_color2" href="tel:5552548458">(555) 254-8458</a>      
+                    </div>
+                </div>
+                <div class="flex-container">
+                    <p>OPEN HOURS</p>
+                    <div class="hero-icons flex-container">
+                        <i class="fas fa-clock"></i>
+                        <p>M-F: 8am - 6pm</p>
+                    </div>
+                    <div class="hero-icons flex-container">
+                        <i class="fas fa-clock"></i>
+                        <p>S-S: 8.30am - 4pm</p>      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+    <section class='contact-form'>
+        <div class='container flex-container'>
+            <form class="flex-container" id='contactForm' method='post' action='contact.php'>
+                <div class='contact-row flex-container'>
+                    <div>
+                        <div id="turning-square-circle1"></div>
+                        <input id='form-contact-input1' type='text' name='name' placeholder="Your name" required>
+                    </div>
+                    <div>
+                        <div id="turning-square-circle2"></div>
+                        <input id='form-contact-input2' type='email' name='email' placeholder="Your email" required>
+                    </div>
+                </div>
+                <div id="turning-square-circle3"></div>
+                <textarea id='form-contact-textarea' name="message" rows="8" placeholder="Message" required></textarea>
+                <input class="services-button" value='SUBMIT' type="submit" name='submit'>
+            </form>
+        </div>
+    </section>
+    <section class='google-map'>
+        <div id='googleMap' class='container flex-container'></div>
+    </section>
+    <section class='faq'>
+        <div class='container flex-container'>
+            <h3>FAQ</h3>
+            <h1>Frequently Asked</h1>
+            <div class='flex-container'>
+                <div class='flex-container'>
+                    <div class='flex-container'>
+                        <h3>MAURIS BLANDIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                    <div class='flex-container'>
+                        <h3>MAURIS BLANDIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                    <div class='flex-container'>
+                        <h3>MAURIS BLANDIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                </div>
+                <div class='flex-container'>
+                    <div class='flex-container'>
+                        <h3>MAURIS BLANDIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                    <div class='flex-container'>
+                        <h3>MAURIS BLANDIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                    <div class='flex-container'>
+                        <h3>MAURIS BLANDIT</h3>
+                        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis por</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
---
+++ views/contact/footer.php
@@ -0,0 +1,33 @@
+<footer class="contacts">
+        <div class="container flex-container">
+            <div class="contacts-box flex-container">
+                <div class="contact-box flex-container">
+                    <img src="images/phone_ve.png" alt="phone_vet">
+                    <h3>Send us a Message</h3>
+                    <a href="mailto:info@thebestvet.com">info@thebestvet.com</a>
+                </div>
+                <div class="contact-box flex-container">
+                    <img src="images/vet_hospital.png" alt="vet_hospital">
+                    <h3>Visit Our Office</h3>
+                    <a href="http://maps.google.com/?q=1234 Green St. Washington DC 95253" target="_blank">1234 Green St. Washington DC 95253</a>
+                </div>
+            </div>
+            <div class="contacts-box flex-container">
+                <div class="contact-box flex-container">
+                    <img src="images/medical_bag.png" alt="medical_bag">
+                    <h3>Call Us Today</h3>
+                    <a href="tel:2354624623">(235) 462-4623</a>
+                </div>
+                <div class="contact-box flex-container">
+                    <img src="images/food.png" alt="animal_food">
+                    <h3>Follow Us</h3>
+                    <div class="social-media-icons flex-container">
+                        <i class="fab fa-facebook fa-2x"></i>
+                        <i class="fab fa-instagram-square fa-2x"></i>
+                        <i class="fab fa-youtube fa-2x"></i>
+                    </div>
+                </div>
+            </div>
+            <p>&copy; Aurimas-stack <?php echo date("Y"); ?></p>
+        </div>
+    </footer>
